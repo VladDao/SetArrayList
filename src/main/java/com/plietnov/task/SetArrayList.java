678fd00ad215a72c0844c7499b6aa7151420889f
Vladyslav Plietnov <vladyslav_plietnov@epam.com>
2019-08-02 14:28:23 +0300

    remove unused import

--- src/main/java/com/plietnov/task/SetArrayList.java
+++ src/main/java/com/plietnov/task/SetArrayList.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SetArrayList<T> extends ArrayList<T> {
 
